217d516f8d1cbde984f2d2e8d4cc9f5ae9d9f0fc
Dave Krisopras Essanto <davemantul123456@gmail.com>
2024-11-06 20:58:37 +0700

    init project

---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+    {
+        author: 'Jeff Kinney',
+        title: 'Hot Mess (Diary of a Wimpy Kid Book 19)',
+        img: img1,
+        id: 1,
+    },
+    {
+        author: 'Stephen R. Covey',
+        title: 'The 7 Habits of Highly Effective People',
+        img: img2,
+        id: 2,
+    },
+    {
+        author: 'Freida McFadden',
+        title: 'The Housemaid ',
+        img: img3,
+        id: 3,
+    },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import books from './books';
+import Book from './Book';
+
+const BookList = () => {
+    return (
+        <>
+            <h1>Amazon Best Sellers</h1>
+            <section className='booklist'>
+                {/* <EventExamples /> */}
+                {books.map((book, index) => {
+                    return <Book {...book} key={book.id} number={index} />
+                })}
+            </section>
+        </>
+    );
+
+
+};
+
+// const EventExamples = () => {
+//     const handleFormInput = (e) =>{
+//         console.log(e.target);
+//         console.log(e.target.name);
+//         console.log(e.target.value);
+//     };
+//     const handleButtonClick = () => {
+//         alert('handle button click');
+//     };
+//     const handleFormSubmission = (e) => {
+//         e.preventDefault();
+//         console.log('form submitted');
+
+//     };
+//     return <section>
+//         <form >
+//             <h2>Typical Form</h2>
+//             <input type="text" name='product' onChange={handleFormInput} style={{margin:'1rem 0'}} />
+//             <button type='submit' onClick={handleFormSubmission}>Submit Form</button>
+//         </form>
+//         <button onClick={handleButtonClick} type='button'>Click Me</button>
+//     </section>
+// };
+
+
+
+
+
+// function Greeting() {
+//     return (
+//         <div>
+//             <Person />
+//             <Message />
+//         </div>
+//     );
+// };
+
+// const Person = () => <h2>Dave Krisopras Essanto</h2>;
+// const Message = () => {
+//     return <p>This is my message</p>
+// }
+
+// function Greeting(){
+//     return React.createElement('h2', {}, 'Hello World!');
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
